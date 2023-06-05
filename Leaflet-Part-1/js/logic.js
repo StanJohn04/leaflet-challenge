8176c61a85f43b5c80d409c3740d46d4c948d7e7
StanJohn04 <stanjohn04@gmail.com>
2023-06-05 15:34:07 -0600

    added legend and popups to map

--- Leaflet-Part-1/js/logic.js
+++ Leaflet-Part-1/js/logic.js
@@ -6,6 +6,7 @@ d3.json(geojsonData).then(function(geoJson) {
     console.log(geoJson)
 });
 
+//function to create map object
 function createMap(earthquakes) {
 
     var streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -28,13 +29,49 @@ function createMap(earthquakes) {
     });
 
     L.control.layers(baseLayers, overlays).addTo(mymap);
+
+// Create a legend to display information about our map.
+    var info = L.control({
+        position: "bottomright"
+    });
+    
+    // When the layer control is added, insert a div with the class of 'info' && "legend".
+    info.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        var depths = [-10, 10, 30, 50, 70, 90];
+
+        // title for legend
+        div.innerHTML += "<h3>Earthquake Depth</h3>"
+
+        //loop through depths to add color hex and label in legend (ex. [lightgreen]10-30)
+        for (var i = 0; i < depths.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(depths[i]) + '"></i> ' +
+                depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    // Add the info legend to the map.
+    info.addTo(mymap);
+  
 }
 
+// function to pick color for color squares in legend
+function getColor(d) {
+    return d >= 90 ? 'rgb(168, 9, 9)' :
+           d >= 70  ? 'rgb(238, 104, 55)' :
+           d >= 50  ? 'orange' :
+           d >= 30  ? 'yellow' :
+           d >= 10   ? 'greenyellow' :
+                      'green';
+}
+
+// function to pick color for each marker
 function markerColor(depth) {
-    if (depth <=10 ) {
+    if (depth <10 ) {
         color = "green"
     }
-    else if (depth > 10 && depth <= 30) {
+    else if (depth >= 10 && depth <= 30) {
         color = "greenyellow"
     }
     else if (depth > 30 && depth <= 50) {
@@ -44,7 +81,7 @@ function markerColor(depth) {
         color = "orange"
     }
     else if (depth > 70 && depth <= 90) {
-        color = "orangered"
+        color = "darkorange"
     }
     else if (depth > 90) {
         color = "red"
@@ -53,26 +90,35 @@ function markerColor(depth) {
     return color
 }
 
+// function to create a marker for each earthquake in data set
 function createMarkers(response) {
+
+    //pull all quake data
     var quakes = response.features;
 
+    //initialize variable to hold markers
     var quakeMarkers = [];
 
+    //loop through quake data and create marker for each one
     for (let i = 0; i < quakes.length; i ++) {
         var quake = quakes[i];
 
+        //cirle at each quake coords, color = depth, radius = magnitude
         var quakeMarker = L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
             color:markerColor(quake.geometry.coordinates[2]),
             fillColor:markerColor(quake.geometry.coordinates[2]),
             fillOpacity:0.5,
             radius:quake.properties.mag * 10000
-        })
+        }).bindPopup(`<h1>${quake.properties.place}</h1> <h3>Lat: ${quake.geometry.coordinates[1]}</h3><h3>Lon: ${quake.geometry.coordinates[0]}</h3><h3>Magnitude: ${quake.properties.mag}</h3><h3>Depth: ${quake.geometry.coordinates[2]}</h3> `)
 
+        //add subset to array
         quakeMarkers.push(quakeMarker);
     }
+    //call createMap function
     createMap(L.layerGroup(quakeMarkers));
 }
 
+//API request and createMarkers function call
 d3.json(geojsonData).then(function(data) {
     createMarkers(data);
 });
